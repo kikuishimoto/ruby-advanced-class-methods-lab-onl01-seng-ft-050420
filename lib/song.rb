8d8e56700857fd5331774718d01ae7824c25224e
kiku ishimoto <kikuish@gmail.com>
2020-05-13 18:50:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,6 +33,7 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       find_by_name(name)
+      puts name
     else
       self.create_by_name(name)
       puts name
